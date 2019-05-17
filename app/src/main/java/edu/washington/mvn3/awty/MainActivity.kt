9939564be7f19f6081240f146cf511f9f1967f76
matthewngo <mvn3@uw.edu>
2019-05-17 12:07:58 -0700

    SMS messaging complete

--- app/src/main/java/edu/washington/mvn3/awty/MainActivity.kt
+++ app/src/main/java/edu/washington/mvn3/awty/MainActivity.kt
@@ -1,12 +1,16 @@
 package edu.washington.mvn3.awty
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.telephony.SmsManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -24,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         val startButton = findViewById<Button>(R.id.startButton)
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(this, AlarmReceiver::class.java)
+        if (checkCallingOrSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), 1)
+        }
 
         startButton.setOnClickListener {
             if (startButton.text == "Start") {
@@ -33,7 +40,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     startButton.text = "Stop"
                     intent.putExtra("Message", messageText.text.toString())
-                    intent.putExtra("Phone", formatPhone(phoneText.text.toString()))
+                    intent.putExtra("Phone", phoneText.text.toString())
                     val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
                     val interval = minText.text.toString().toLong() * 60000
                     alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, alarmIntent)
@@ -46,15 +53,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun formatPhone(phone: String): String {
-        return "(" + phone.substring(0, 3) + ")" + phone.substring(3, 6) + "-" + phone.substring(6)
-    }
-
     class AlarmReceiver: BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val caption = "Texting " + intent!!.getStringExtra("Phone") + "\n"
-            val message = intent!!.getStringExtra("Phone") + ":" + intent!!.getStringExtra("Message")
+            val caption = "Texting " + formatPhone(intent!!.getStringExtra("Phone")) + "\n"
+            val message = formatPhone(intent!!.getStringExtra("Phone")) + ":" + intent!!.getStringExtra("Message")
             Toast.makeText(context, caption + message, Toast.LENGTH_SHORT).show()
+            println("PHONE: " + intent!!.getStringExtra("Phone"))
+            SmsManager.getDefault().sendTextMessage(intent!!.getStringExtra("Phone"), null, message, null, null)
+        }
+
+        private fun formatPhone(phone: String): String {
+            return "(" + phone.substring(0, 3) + ")" + phone.substring(3, 6) + "-" + phone.substring(6)
         }
     }
 }
